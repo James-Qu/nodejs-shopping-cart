da9727fc9ef3c940388a9f9cfb57ae98581c300d
james.qu <qusz88@gmail.com>
2020-03-29 23:12:25 -0500

    add add-to-cart route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 
 /* GET home page. */
 router.get('/', (req, res) => {
@@ -14,6 +14,21 @@ router.get('/', (req, res) => {
       productChunks.push(docs.slice(i, i + chunkSize));
     }
     res.render('shop/index', { title: 'Shopping Cart', products: productChunks });
+  });
+});
+
+router.get('/add-to-cart/:id', (req, res) => {
+  const productId = req.params.id;
+  const cart = new Cart(req.session.cart ? req.session.cart : { });
+
+  Product.findById(productId, (err, product) => {
+    if (err) {
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    req.session.cart = cart;
+    console.log(req.session);
+    res.redirect('/');
   });
 });
 
